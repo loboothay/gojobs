44ece0c48c10fd507e14b1b92028e47cc5367489
Thaynara Lobo <loboo.thaynara@gmail.com>
2023-04-11 06:53:28 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/loboothay/gojobs.git/router"
+import (
+	"github.com/loboothay/gojobs.git/config"
+	"github.com/loboothay/gojobs.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
 	//Initiando o Router
 	router.Initialize()
 }
