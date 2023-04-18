71000d4af99d9e5500b19cd50ed15072e671d0ce
Thaynara Lobo <loboo.thaynara@gmail.com>
2023-04-18 17:22:26 -0300

    feat: add update opening handler logic

--- handler/request.go
+++ handler/request.go
@@ -40,3 +40,22 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+//update
+type UpdateOpeningRequest struct {
+	Role     string
+	Company  string
+	Location string
+	Remote   *bool
+	Link     string
+	Salary   int64
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// If any field is provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// If none of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
+}
